32404b2d8e1eb236f53aeb45d381e8f5064716f3
davien21 <chidiebereekennia@gmail.com>
2020-10-27 16:03:16 +0100

    build linked lists with buckets-js

---
+++ linked-lists.js
@@ -0,0 +1,10 @@
+const { LinkedList } = require('buckets-js')
+
+let list = new LinkedList();
+list.add(10)
+list.add(20)
+list.add(30)
+console.log(list.toArray())
+console.log(list.size())
+console.log(list.contains(10))
+console.log(list.indexOf(10))
